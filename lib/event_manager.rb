db1ea9d8c3e0d2d8819674f538a658a6a9146986
ShortFir <shortfir@protonmail.com>
2021-05-05 13:56:50 +1000

    Continueing with the tutorial.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,42 @@
 # frozen_string_literal: false
 
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'EventManager Initialized!'
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index.zero?
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
 
-  columns = line.split(',')
-  name = columns[2]
-  puts name
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(', ')
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
